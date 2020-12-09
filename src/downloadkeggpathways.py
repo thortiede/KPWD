719e6246d47d3aa736b59feba45002bda98d0d7e
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2020-12-09 11:31:08 +0100

    Change software name in licence header

--- src/downloadkeggpathways.py
+++ src/downloadkeggpathways.py
@@ -4,7 +4,7 @@
  * --------------------------------------------------------------------------
  * University of Tuebingen, 2020.
  *
- * This code is part of the SBML4j software package and subject to the terms
+ * This code is part of the KPWD software package and subject to the terms
  * and conditions defined by its license (MIT License). For license details
  * please refer to the LICENSE file included as part of this source code
  * package.
